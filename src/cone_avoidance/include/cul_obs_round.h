196e18adf5f6e235dd2e345c386d1c86e4299db6
RgerColdline <2089378601@qq.com>
2025-12-21 13:39:04 +0800

    每次计算前清空

--- src/cone_avoidance/include/cul_obs_round.h
+++ src/cone_avoidance/include/cul_obs_round.h
@@ -15,26 +15,27 @@ struct ObsRound
 std::vector<ObsRound> obs_rounds;
 
 void transObs(const std::vector<Obstacle>& obss){
+    obs_rounds.clear();
     for(auto &obs:obss){
-            ObsRound obs_round;
-            obs_round.position=obs.position;
-            obs_round.radius=obs.radius;
-            obs_round.safe_radius=obs.radius+UAV_radius;
-            Eigen::Vector2f dist_vec=obs.position-current_pos;
-            float dist=dist_vec.norm();
-            Eigen::Vector2f radius_vec=dist_vec/dist*obs_round.safe_radius;
-            if(dist<=obs_round.safe_radius){
-                Eigen::Rotation2D<float> rot(M_PI/2);
-                Eigen::Vector2f close_point=obs.position-radius_vec;
-                obs_round.left_point=rot*radius_vec/2+close_point;
-                obs_round.right_point=rot.inverse()*radius_vec/2+close_point;
-            }else{
-                float angle=asin(obs_round.safe_radius/dist);
-                Eigen::Rotation2D<float> rot(M_PI/2-angle);
-                obs_round.right_point=rot*(-radius_vec)+obs.position;
-                rot=Eigen::Rotation2D<float>(-M_PI/2+angle);
-                obs_round.left_point=rot*(-radius_vec)+obs.position;
-            }
-            obs_rounds.push_back(obs_round);
+        ObsRound obs_round;
+        obs_round.position=obs.position;
+        obs_round.radius=obs.radius;
+        obs_round.safe_radius=obs.radius+UAV_radius;
+        Eigen::Vector2f dist_vec=obs.position-current_pos;
+        float dist=dist_vec.norm();
+        Eigen::Vector2f radius_vec=dist_vec/dist*obs_round.safe_radius;
+        if(dist<=obs_round.safe_radius){
+            Eigen::Rotation2D<float> rot(M_PI/2);
+            Eigen::Vector2f close_point=obs.position-radius_vec;
+            obs_round.left_point=rot*radius_vec/2+close_point;
+            obs_round.right_point=rot.inverse()*radius_vec/2+close_point;
+        }else{
+            float angle=asin(obs_round.safe_radius/dist);
+            Eigen::Rotation2D<float> rot(M_PI/2-angle);
+            obs_round.right_point=rot*(-radius_vec)+obs.position;
+            rot=Eigen::Rotation2D<float>(-M_PI/2+angle);
+            obs_round.left_point=rot*(-radius_vec)+obs.position;
         }
+        obs_rounds.push_back(obs_round);
+    }
 }
